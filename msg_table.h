072af51589b9ae10aa5f574d578ae981b87c68a6
jsbruglie <joao.borrego64@gmail.com>
2017-02-26 13:06:31 +0000

    Separating message table from msgserv util functions

---
+++ msg_table.h
@@ -0,0 +1,36 @@
+#ifndef MSG_TABLE
+#define MSG_TABLE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Message struct type */
+typedef struct _Message{
+	char text[140];
+	int clock;
+}Message;
+
+/* Message Table struct type */
+typedef struct _MessageTable{
+	int size;
+	int count;
+	Message** table;
+}MessageTable;
+
+/* Functions */
+
+Message** create_msg_table(int size);
+void delete_msg_table(Message** msg_table, int size);
+int get_msg_clock(Message** msg_table, int i);
+void swap_msg(Message** msg_table, int a, int b);
+
+/* Quick Sort */
+void sort_msg_table(Message** msg_table, int size, int items);
+void quick_sort(Message** msg_table, int l, int r);
+int partition(Message** msg_table, int l, int r, int pivot);
+
+/* DEBUG */
+void print_msg_table(Message** msg_table, int size);
+
+#endif
--- msgserv_utils.h
+++ msgserv_utils.h
@@ -12,34 +12,14 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
+# include "msg_table.h"
 #include "debug.h"
-
-/* Message struct type */
-typedef struct _Message{
-	char text[140];
-	int clock;
-}Message;
 
 /* Functions */
 
-/* Message Server */
 void parse_args(int argc, char** argv, char** _name, char** _ip, int* _upt, int* _tpt,
 				char** _siip, int* _sipt, int* _m, int* _r);
 
 int create_udp_server(u_short port);
-
-/* Message Table */
-Message** create_msg_table(int size);
-void delete_msg_table(Message** msg_table, int size);
-
-/* Quick Sort */
-void quick_sort(Message** msg_table, int l, int r);
-int partition(Message** msg_table, int l, int r, int pivot);
-void sort_msg_table(Message** msg_table, int size, int items);
-int get_msg_clock(Message** msg_table, int i);
-void swap_msg(Message** msg_table, int a, int b);
-
-/* DEBUG */
-void print_msg_table(Message** msg_table, int size);
 
 #endif
